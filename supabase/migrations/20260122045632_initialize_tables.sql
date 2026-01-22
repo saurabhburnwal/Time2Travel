82dcba1855e82bdcfb989f743b7a8ef971f73d49
Saurabh <saurabhburnwal00@gmail.com>
2026-01-22 10:26:47 +0530

    intialize database

---
+++ supabase/migrations/20260122045632_initialize_tables.sql
@@ -0,0 +1,158 @@
+
+  create table "public"."orders" (
+    "id" uuid not null default gen_random_uuid(),
+    "user_id" uuid,
+    "total_amount" numeric not null,
+    "created_at" timestamp with time zone default now()
+      );
+
+
+
+  create table "public"."users" (
+    "id" uuid not null default gen_random_uuid(),
+    "name" text not null,
+    "email" text not null,
+    "created_at" timestamp with time zone default now()
+      );
+
+
+alter table "public"."users" enable row level security;
+
+CREATE UNIQUE INDEX orders_pkey ON public.orders USING btree (id);
+
+CREATE UNIQUE INDEX users_email_key ON public.users USING btree (email);
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."orders" add constraint "orders_pkey" PRIMARY KEY using index "orders_pkey";
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."orders" add constraint "orders_user_id_fkey" FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."orders" validate constraint "orders_user_id_fkey";
+
+alter table "public"."users" add constraint "users_email_key" UNIQUE using index "users_email_key";
+
+grant delete on table "public"."orders" to "anon";
+
+grant insert on table "public"."orders" to "anon";
+
+grant references on table "public"."orders" to "anon";
+
+grant select on table "public"."orders" to "anon";
+
+grant trigger on table "public"."orders" to "anon";
+
+grant truncate on table "public"."orders" to "anon";
+
+grant update on table "public"."orders" to "anon";
+
+grant delete on table "public"."orders" to "authenticated";
+
+grant insert on table "public"."orders" to "authenticated";
+
+grant references on table "public"."orders" to "authenticated";
+
+grant select on table "public"."orders" to "authenticated";
+
+grant trigger on table "public"."orders" to "authenticated";
+
+grant truncate on table "public"."orders" to "authenticated";
+
+grant update on table "public"."orders" to "authenticated";
+
+grant delete on table "public"."orders" to "postgres";
+
+grant insert on table "public"."orders" to "postgres";
+
+grant references on table "public"."orders" to "postgres";
+
+grant select on table "public"."orders" to "postgres";
+
+grant trigger on table "public"."orders" to "postgres";
+
+grant truncate on table "public"."orders" to "postgres";
+
+grant update on table "public"."orders" to "postgres";
+
+grant delete on table "public"."orders" to "service_role";
+
+grant insert on table "public"."orders" to "service_role";
+
+grant references on table "public"."orders" to "service_role";
+
+grant select on table "public"."orders" to "service_role";
+
+grant trigger on table "public"."orders" to "service_role";
+
+grant truncate on table "public"."orders" to "service_role";
+
+grant update on table "public"."orders" to "service_role";
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "postgres";
+
+grant insert on table "public"."users" to "postgres";
+
+grant references on table "public"."users" to "postgres";
+
+grant select on table "public"."users" to "postgres";
+
+grant trigger on table "public"."users" to "postgres";
+
+grant truncate on table "public"."users" to "postgres";
+
+grant update on table "public"."users" to "postgres";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
+  create policy "Users can read their own data"
+  on "public"."users"
+  as permissive
+  for select
+  to public
+using ((auth.uid() = id));
+
+
+
